c6472a74ff709dbe407b1adb4b6d2d4c2b46b1f3
Ulfa Choiriyah <ulfacho63@gmail.com>
2025-11-21 20:02:47 +0700

    Menambahkan file app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+
+pages = [
+    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
+    st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
+]
+
+pg = st.navigation(
+    pages, 
+    position="sidebar",
+    expanded=True
+)
+
+pg.run()
